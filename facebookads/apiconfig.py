416617d08d6ee74e1256b4c929d734e39c9228b3
Alon Diamant <alon@selectom.com>
2018-07-31 18:54:52 +0300

    Updated version to 2017.7.31 Using Facebook API v2.12

--- facebookads/apiconfig.py
+++ facebookads/apiconfig.py
@@ -19,7 +19,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 ads_api_config = {
-  'API_VERSION': 'v2.11',
-  'SDK_VERSION': 'v2.11.4',
+  'API_VERSION': 'v2.12',
+  'SDK_VERSION': 'v2018.7.31',
   'STRICT_MODE': False
 }
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ readme_filename = os.path.join(this_dir, 'README.md')
 requirements_filename = os.path.join(this_dir, 'requirements.txt')
 
 PACKAGE_NAME = 'facebookads'
-PACKAGE_VERSION = '2018.04.21'
+PACKAGE_VERSION = '2018.7.31'
 PACKAGE_AUTHOR = 'Facebook'
 PACKAGE_AUTHOR_EMAIL = ''
 PACKAGE_URL = 'https://github.com/facebook/facebook-python-ads-sdk'
